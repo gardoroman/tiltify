5ed256ed0770b9d5bba9817ff742b2a42aa3d390
Edgardo Roman <eroman.transporttech.io>
2021-05-09 15:27:02 -0500

    Add message for proper format.

--- lib/tiltify_web/views/error_view.ex
+++ lib/tiltify_web/views/error_view.ex
@@ -1,21 +1,23 @@
 defmodule TiltifyWeb.ErrorView do
   use TiltifyWeb, :view
 
+  @proper_format "Please format request properly, e.g. rates/EUR/10/USD"
+
   def render("404.html", _assigns) do
     "Page not found"
   end
 
   def render("error.html", %{info: message}) do
-    message
+    message <> " " <> @proper_format
   end
 
   def render("unsupported.html", %{info: base}) do
     "Base #{base} not supported" <>
-    "The service only supports EUR at the moment." 
+    "The service only supports EUR at the moment."
   end
 
   def render("unknown.html", %{info: target}) do
-    "Target: #{target} is unknown" 
+    "Target: #{target} is unknown " <> @proper_format
   end  
 
 end
